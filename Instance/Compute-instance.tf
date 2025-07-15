8b01e55f698b3b783e85de3e755d917f6305a24d
SurgingSparks <joclopez@sice.com.au>
2025-07-15 22:50:24 +1000

    adding provider version

--- Instance/Compute-instance.tf
+++ Instance/Compute-instance.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = ">= 6.34.0"
+    }
+  }
+}
+
+provider "oci" {}
 resource "oci_core_instance" "basic_platform_vm1" {
     availability_domain = var.availability_domain
     compartment_id      = var.compartment_ocid
