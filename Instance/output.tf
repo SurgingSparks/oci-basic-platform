12f7f318d7b126580693dfeee9ee8ee5bf33c6ec
joclopez <joclopez@sice.com.au>
2025-07-17 22:15:04 +1000

    edit output

--- Instance/output.tf
+++ Instance/output.tf
@@ -5,7 +5,3 @@ output "instance_ocid" {
 output "instance_public_ip" {
     value = oci_core_instance.basic_platform_vm1.public_ip
 }
-
-output "block_volume_ocid" {
-    value = oci_core_volume.basic_platform_vm1_data.id
-}
