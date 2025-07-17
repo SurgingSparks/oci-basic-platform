5012940f941087338159f55683c37e68b806ad73
joclopez <joclopez@sice.com.au>
2025-07-17 22:27:53 +1000

    removing volume attachment here

--- Instance_template/main.tf
+++ Instance_template/main.tf
@@ -78,20 +78,7 @@ resource "oci_core_instance" "generated_oci_core_instance" {
 		are_legacy_imds_endpoints_disabled = "false"
 	}
 	is_pv_encryption_in_transit_enabled = "true"
-	launch_volume_attachments {
-		device = "/dev/oracleoci/oraclevdb"
-		display_name = "basic-platform-vm1-data"
-		is_read_only = "false"
-		is_shareable = "false"
-		launch_create_volume_details {
-			compartment_id = "ocid1.compartment.oc1..aaaaaaaajzauiwdqmhulsdzco7zmwdeiz54foiecsnteo2ztmip42dhbcaja"
-			display_name = "basic-platform-vm1-data"
-			size_in_gbs = "50"
-			volume_creation_type = "ATTRIBUTES"
-			vpus_per_gb = "10"
-		}
-		type = "paravirtualized"
-	}
+
 	metadata = {
 		"ssh_authorized_keys" = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCpyDicYVhqV4OSJbMKG9MuUW5pA2I7mw7yAU3OzxU3QQjaHXVmhPYTXzh+IGFVsXn5tnIkPMC3jtNNwDSTDy54DFNVUSbi6oJISofXnjVL25VIvSg7MQYeAZgfhbzopgF6UZOElKgvaw99QypiBkenSkko0T7jef3UOpzyj0Q1j8L4k3gEL1tFmXsgnGD7/TGcXJRc4J7WwQqUH8/SVlLXS33l7IwPgr4giBCk30d0dnaJEiYDNGk0aB0C9UXBZdK3sjg4syFvjiOpbCUcZVV8jok8cbNO2Hw/4yxfhTEN635u+xmDwWtOfwnqrAazwn5Umuh+L1jUmNYTZsuPyMvN ssh-key-2025-07-15"
 	}
