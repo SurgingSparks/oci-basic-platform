c9d4f111dfc6ee17a6dcc56b29bcac200f00fe3a
joclopez <joclopez@sice.com.au>
2025-07-22 23:21:40 +1000

    update to naming convention

--- Instance/Variables.tf
+++ Instance/Variables.tf
@@ -1,6 +1,6 @@
-#instance
+# General instance settings
 variable "availability_domain" {
-  description = "The AD (Availability Domain) where the instance will be launched."
+  description = "The AD (Availability Domain) where the instances will be launched."
   type        = string
   default     = "gloC:AP-SYDNEY-1-AD-1"
 }
@@ -9,12 +9,6 @@ variable "compartment_id" {
   description = "The OCID of the compartment to create resources in."
   type        = string
   default     = "ocid1.compartment.oc1..aaaaaaaajzauiwdqmhulsdzco7zmwdeiz54foiecsnteo2ztmip42dhbcaja"
-}
-
-variable "display_name" {
-  description = "The display name of the instance."
-  type        = string
-  default     = "basic-platform-vm1"
 }
 
 variable "shape" {
@@ -27,12 +21,6 @@ variable "subnet_id" {
   description = "The OCID of the subnet to attach the VNIC."
   type        = string
   default     = "ocid1.subnet.oc1.ap-sydney-1.aaaaaaaawkqfehmuvpd7ymvw7canszq2qsck3gilpzfn3kke3ai6nhzt3hja"
-}
-
-variable "basic_platform_vm1_private_ip" {
-  description = "Static private IP address to assign to the primary VNIC."
-  type        = string
-  default     = "10.42.0.10"
 }
 
 variable "ssh_authorized_keys" {
@@ -101,115 +89,125 @@ variable "are_legacy_imds_endpoints_disabled" {
   default     = false
 }
 
-#vm2
-variable "basic_platform_vm2_display_name" {
-  description = "The display name of the VM2 instance."
+# VM names & IPs
+variable "vm_basic_platform_web_display_name" {
+  description = "The display name of the web instance."
   type        = string
-  default     = "basic-platform-vm2"
+  default     = "vm-basic-platform-web"
 }
 
-variable "basic_platform_vm2_private_ip" {
-  description = "Static private IP address for VM2."
+variable "vm_basic_platform_web_private_ip" {
+  description = "Static private IP address for the web instance."
+  type        = string
+  default     = "10.42.0.10"
+}
+
+variable "vm_basic_platform_app_display_name" {
+  description = "The display name of the app instance."
+  type        = string
+  default     = "vm-basic-platform-app"
+}
+
+variable "vm_basic_platform_app_private_ip" {
+  description = "Static private IP address for the app instance."
   type        = string
   default     = "10.42.0.11"
 }
 
-
-# vm1 storage
-
-variable "basic_platform_vm1_logs_volume_display_name" {
-  description = "Display name for the logs block volume."
+# web storage
+variable "basic_platform_web_logs_volume_display_name" {
+  description = "Display name for the web logs block volume."
   type        = string
-  default     = "vol-basic-platform-vm1-logs"
+  default     = "vol-basic-platform-web-logs"
 }
 
-variable "basic_platform_vm1_logs_volume_size_in_gbs" {
-  description = "Size of the logs block volume in GB."
+variable "basic_platform_web_logs_volume_size_in_gbs" {
+  description = "Size of the web logs block volume in GB."
   type        = number
   default     = 50
 }
 
-variable "basic_platform_vm1_logs_volume_vpus_per_gb" {
-  description = "VPUs per GB for the logs block volume."
+variable "basic_platform_web_logs_volume_vpus_per_gb" {
+  description = "VPUs per GB for the web logs block volume."
   type        = number
   default     = 10
 }
 
-variable "basic_platform_vm1_logs_attachment_device" {
-  description = "Device name for the logs block volume attachment."
+variable "basic_platform_web_logs_attachment_device" {
+  description = "Device name for the web logs block volume attachment."
   type        = string
   default     = "/dev/oracleoci/oraclevdb"
 }
 
-variable "basic_platform_vm1_logs_attachment_display_name" {
-  description = "Display name for the logs block volume attachment."
+variable "basic_platform_web_logs_attachment_display_name" {
+  description = "Display name for the web logs block volume attachment."
   type        = string
-  default     = "vola-basic-platform-vm1-logs"
+  default     = "vola-basic-platform-web-logs"
 }
 
-variable "basic_platform_vm1_logs_is_read_only" {
-  description = "Whether the logs block volume attachment is read-only."
+variable "basic_platform_web_logs_is_read_only" {
+  description = "Whether the web logs block volume attachment is read-only."
   type        = bool
   default     = false
 }
 
-variable "basic_platform_vm1_logs_is_shareable" {
-  description = "Whether the logs block volume attachment is shareable."
+variable "basic_platform_web_logs_is_shareable" {
+  description = "Whether the web logs block volume attachment is shareable."
   type        = bool
   default     = false
 }
 
-variable "basic_platform_vm1_logs_is_pv_encryption_in_transit_enabled" {
-  description = "Enable encryption in transit for the logs block volume attachment."
+variable "basic_platform_web_logs_is_pv_encryption_in_transit_enabled" {
+  description = "Enable encryption in transit for the web logs block volume attachment."
   type        = bool
   default     = true
 }
 
-#vm2 storage
-variable "basic_platform_vm2_logs_volume_display_name" {
-  description = "Display name for the VM2 logs block volume."
+# app storage
+variable "basic_platform_app_logs_volume_display_name" {
+  description = "Display name for the app logs block volume."
   type        = string
-  default     = "vol-basic-platform-vm2-logs"
+  default     = "vol-basic-platform-app-logs"
 }
 
-variable "basic_platform_vm2_logs_volume_size_in_gbs" {
-  description = "Size of the VM2 logs block volume in GB."
+variable "basic_platform_app_logs_volume_size_in_gbs" {
+  description = "Size of the app logs block volume in GB."
   type        = number
   default     = 50
 }
 
-variable "basic_platform_vm2_logs_volume_vpus_per_gb" {
-  description = "VPUs per GB for the VM2 logs block volume."
+variable "basic_platform_app_logs_volume_vpus_per_gb" {
+  description = "VPUs per GB for the app logs block volume."
   type        = number
   default     = 10
 }
 
-variable "basic_platform_vm2_logs_attachment_device" {
-  description = "Device name for the VM2 logs block volume attachment."
+variable "basic_platform_app_logs_attachment_device" {
+  description = "Device name for the app logs block volume attachment."
   type        = string
   default     = "/dev/oracleoci/oraclevdc"
 }
 
-variable "basic_platform_vm2_logs_attachment_display_name" {
-  description = "Display name for the VM2 logs block volume attachment."
+variable "basic_platform_app_logs_attachment_display_name" {
+  description = "Display name for the app logs block volume attachment."
   type        = string
-  default     = "vola-basic-platform-vm2-logs"
+  default     = "vola-basic-platform-app-logs"
 }
 
-variable "basic_platform_vm2_logs_is_read_only" {
-  description = "Whether the VM2 logs block volume attachment is read-only."
+variable "basic_platform_app_logs_is_read_only" {
+  description = "Whether the app logs block volume attachment is read-only."
   type        = bool
   default     = false
 }
 
-variable "basic_platform_vm2_logs_is_shareable" {
-  description = "Whether the VM2 logs block volume attachment is shareable."
+variable "basic_platform_app_logs_is_shareable" {
+  description = "Whether the app logs block volume attachment is shareable."
   type        = bool
   default     = false
 }
 
-variable "basic_platform_vm2_logs_is_pv_encryption_in_transit_enabled" {
-  description = "Enable encryption in transit for the VM2 logs block volume attachment."
+variable "basic_platform_app_logs_is_pv_encryption_in_transit_enabled" {
+  description = "Enable encryption in transit for the app logs block volume attachment."
   type        = bool
   default     = true
 }
--- Instance/basic-platform-instances.tf
+++ Instance/basic-platform-instances.tf
@@ -1,102 +1,6 @@
 provider "oci" {}
 
-resource "oci_core_instance" "basic_platform_vm1" {
-  agent_config {
-    is_management_disabled  = var.is_management_disabled
-    is_monitoring_disabled  = var.is_monitoring_disabled
-    plugins_config {
-      desired_state = "DISABLED"
-      name          = "WebLogic Management Service"
-    }
-    plugins_config {
-      desired_state = "DISABLED"
-      name          = "Vulnerability Scanning"
-    }
-    plugins_config {
-      desired_state = "DISABLED"
-      name          = "Oracle Java Management Service"
-    }
-    plugins_config {
-      desired_state = "DISABLED"
-      name          = "OS Management Hub Agent"
-    }
-    plugins_config {
-      desired_state = "DISABLED"
-      name          = "Management Agent"
-    }
-    plugins_config {
-      desired_state = "ENABLED"
-      name          = "Custom Logs Monitoring"
-    }
-    plugins_config {
-      desired_state = "DISABLED"
-      name          = "Compute RDMA GPU Monitoring"
-    }
-    plugins_config {
-      desired_state = "ENABLED"
-      name          = "Compute Instance Run Command"
-    }
-    plugins_config {
-      desired_state = "ENABLED"
-      name          = "Compute Instance Monitoring"
-    }
-    plugins_config {
-      desired_state = "DISABLED"
-      name          = "Compute HPC RDMA Auto-Configuration"
-    }
-    plugins_config {
-      desired_state = "DISABLED"
-      name          = "Compute HPC RDMA Authentication"
-    }
-    plugins_config {
-      desired_state = "ENABLED"
-      name          = "Cloud Guard Workload Protection"
-    }
-    plugins_config {
-      desired_state = "DISABLED"
-      name          = "Block Volume Management"
-    }
-    plugins_config {
-      desired_state = "DISABLED"
-      name          = "Bastion"
-    }
-  }
-
-  availability_config {
-    recovery_action = "RESTORE_INSTANCE"
-  }
-
-  availability_domain                 = var.availability_domain
-  compartment_id                      = var.compartment_id
-  display_name                        = var.display_name
-  shape                               = var.shape
-  is_pv_encryption_in_transit_enabled = var.is_pv_encryption_in_transit_enabled
-
-  create_vnic_details {
-    assign_ipv6ip                = var.assign_ipv6ip
-    assign_private_dns_record    = var.assign_private_dns_record
-    assign_public_ip             = var.assign_public_ip
-    private_ip                   = var.basic_platform_vm1_private_ip
-    subnet_id                    = var.subnet_id
-  }
-
-  instance_options {
-    are_legacy_imds_endpoints_disabled = var.are_legacy_imds_endpoints_disabled
-  }
-
-  metadata = {
-    ssh_authorized_keys = var.ssh_authorized_keys
-  }
-
-  source_details {
-    boot_volume_size_in_gbs   = var.boot_volume_size_in_gbs
-    boot_volume_vpus_per_gb   = var.boot_volume_vpus_per_gb
-    source_id                 = var.source_image_id
-    source_type               = "image"
-  }
-}
-
-resource "oci_core_instance" "basic_platform_vm2" {
+resource "oci_core_instance" "vm_basic_platform_web" {
   agent_config {
     is_management_disabled  = var.is_management_disabled
     is_monitoring_disabled  = var.is_monitoring_disabled
@@ -164,7 +68,7 @@ resource "oci_core_instance" "basic_platform_vm2" {
 
   availability_domain                  = var.availability_domain
   compartment_id                       = var.compartment_id
-  display_name                         = var.basic_platform_vm2_display_name
+  display_name                         = var.vm_basic_platform_web_display_name
   shape                                = var.shape
   is_pv_encryption_in_transit_enabled = var.is_pv_encryption_in_transit_enabled
 
@@ -172,7 +76,103 @@ resource "oci_core_instance" "basic_platform_vm2" {
     assign_ipv6ip                = var.assign_ipv6ip
     assign_private_dns_record    = var.assign_private_dns_record
     assign_public_ip             = var.assign_public_ip
-    private_ip                   = var.basic_platform_vm2_private_ip
+    private_ip                   = var.vm_basic_platform_web_private_ip
+    subnet_id                    = var.subnet_id
+  }
+
+  instance_options {
+    are_legacy_imds_endpoints_disabled = var.are_legacy_imds_endpoints_disabled
+  }
+
+  metadata = {
+    ssh_authorized_keys = var.ssh_authorized_keys
+  }
+
+  source_details {
+    boot_volume_size_in_gbs   = var.boot_volume_size_in_gbs
+    boot_volume_vpus_per_gb   = var.boot_volume_vpus_per_gb
+    source_id                 = var.source_image_id
+    source_type               = "image"
+  }
+}
+
+resource "oci_core_instance" "vm_basic_platform_app" {
+  agent_config {
+    is_management_disabled  = var.is_management_disabled
+    is_monitoring_disabled  = var.is_monitoring_disabled
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "WebLogic Management Service"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Vulnerability Scanning"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Oracle Java Management Service"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "OS Management Hub Agent"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Management Agent"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Custom Logs Monitoring"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Compute RDMA GPU Monitoring"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Compute Instance Run Command"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Compute Instance Monitoring"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Compute HPC RDMA Auto-Configuration"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Compute HPC RDMA Authentication"
+    }
+    plugins_config {
+      desired_state = "ENABLED"
+      name          = "Cloud Guard Workload Protection"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Block Volume Management"
+    }
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Bastion"
+    }
+  }
+
+  availability_config {
+    recovery_action = "RESTORE_INSTANCE"
+  }
+
+  availability_domain                  = var.availability_domain
+  compartment_id                       = var.compartment_id
+  display_name                         = var.vm_basic_platform_app_display_name
+  shape                                = var.shape
+  is_pv_encryption_in_transit_enabled = var.is_pv_encryption_in_transit_enabled
+
+  create_vnic_details {
+    assign_ipv6ip                = var.assign_ipv6ip
+    assign_private_dns_record    = var.assign_private_dns_record
+    assign_public_ip             = var.assign_public_ip
+    private_ip                   = var.vm_basic_platform_app_private_ip
     subnet_id                    = var.subnet_id
   }
 
