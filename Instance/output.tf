ad9db153913367533facd66f5d1126b8aea6db80
joclopez <joclopez@sice.com.au>
2025-07-22 23:47:13 +1000

    output

--- Instance/output.tf
+++ Instance/output.tf
@@ -1,33 +1,41 @@
-output "instance_id" {
-  value = oci_core_instance.basic_platform_vm1.id
-}
-output "instance_public_ip" {
-  value = oci_core_instance.basic_platform_vm1.public_ip
-}
-output "instance_private_ip" {
-  value = oci_core_instance.basic_platform_vm1.private_ip
-}
-output "basic_platform_vm1_logs_volume_id" {
-  value = oci_core_volume.vol-basic-platform-vm1-logs.id
-}
-output "basic_platform_vm1_logs_volume_attachment_id" {
-  value = oci_core_volume_attachment.vola-basic-platform-vm1-logs.id
-}
-output "basic_platform_vm2_instance_id" {
-  value = oci_core_instance.basic_platform_vm2.id
+# Web instance outputs
+output "vm_basic_platform_web_instance_id" {
+  value = oci_core_instance.vm_basic_platform_web.id
 }
 
-output "basic_platform_vm2_instance_public_ip" {
-  value = oci_core_instance.basic_platform_vm2.public_ip
+output "vm_basic_platform_web_instance_public_ip" {
+  value = oci_core_instance.vm_basic_platform_web.public_ip
 }
 
-output "basic_platform_vm2_instance_private_ip" {
-  value = oci_core_instance.basic_platform_vm2.private_ip
-}
-output "basic_platform_vm2_logs_volume_id" {
-  value = oci_core_volume.vol-basic-platform-vm2-logs.id
+output "vm_basic_platform_web_instance_private_ip" {
+  value = oci_core_instance.vm_basic_platform_web.private_ip
 }
 
-output "basic_platform_vm2_logs_volume_attachment_id" {
-  value = oci_core_volume_attachment.vola-basic-platform-vm2-logs.id
+output "basic_platform_web_logs_volume_id" {
+  value = oci_core_volume.vol-basic-platform-web-logs.id
+}
+
+output "basic_platform_web_logs_volume_attachment_id" {
+  value = oci_core_volume_attachment.vola-basic-platform-web-logs.id
+}
+
+# App instance outputs
+output "vm_basic_platform_app_instance_id" {
+  value = oci_core_instance.vm_basic_platform_app.id
+}
+
+output "vm_basic_platform_app_instance_public_ip" {
+  value = oci_core_instance.vm_basic_platform_app.public_ip
+}
+
+output "vm_basic_platform_app_instance_private_ip" {
+  value = oci_core_instance.vm_basic_platform_app.private_ip
+}
+
+output "basic_platform_app_logs_volume_id" {
+  value = oci_core_volume.vol-basic-platform-app-logs.id
+}
+
+output "basic_platform_app_logs_volume_attachment_id" {
+  value = oci_core_volume_attachment.vola-basic-platform-app-logs.id
 }
